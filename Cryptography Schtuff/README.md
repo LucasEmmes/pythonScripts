f9bbca1c34aab9d2ae59a453c7bba5b9af5b6d80
LucasEmmes <79790657+LucasEmmes@users.noreply>
2021-03-03 22:35:22 +0100

    Added transpotition
    Added transposition.py (bc I apparently forgot last commit), and updated README

--- Cryptography Schtuff/README.md
+++ Cryptography Schtuff/README.md
@@ -1,24 +1,24 @@
 ## Cryptography Stuff, yay! ##
 
-I took a class of cryptography in the spring of 2021  
+I took a class of cryptography in the spring of 2021,  
 and when we went through some historical ciphers I thought  
 to myself: these can't be all that hard to replicate in python.  
-And they really weren't. Still took me some amount of time though.
+And they really weren't. Still kinda tricky.
 
 Most of these (with the exception of visual.py)  
 are meant to be used as an import, but if you want  
-to run them in a console, you can.
+to run them by themselves in a console, you can.
 
 All of these are delivered **as is**.  
 This just means I have not gotten around to  
 implementing good failsafes / debugging code  
-yet in case something were to go wrong  
-or in the case of incorrect input.
+yet in case something were to go wrong.
 
 *Notes*
   - visual.py  
-    This one doesn't have docstrings yet  
-    but they are prob not really needed either.
+	This one requires you have Pillow installed.
+    It also doesn't have docstrings yet  
+    but probably aren't needed either.
     
  - substitution.py and transposition.py  
     Take note when making alphabets for these to include **EVERY**  
---
+++ Cryptography Schtuff/transposition.py
@@ -0,0 +1,78 @@
+def ceasar_cipher(text, alphabet, k, code):
+    """text (string): The text you would like to encode or decode
+    alphabet (array[char]/string): List of all the available characters in order
+        May be one long string, or array of single chars
+    k (int): The encryption key
+    code (int): Whether you want to encode(0) or decode(1)"""
+
+    if code == 0:
+
+        encodingDictionairy = {}
+        
+        for i in range(0, len(alphabet)):
+            encodingDictionairy[alphabet[i]] = alphabet[(i+k)%len(alphabet)]
+
+        plaintext = text
+        ciphertext = ""
+
+        for plainChar in plaintext:
+            ciphertext += encodingDictionairy[plainChar]
+
+        return ciphertext
+
+    elif code == 1:
+
+        decodingDictionairy = {}
+
+        for i in range(0, len(alphabet)):
+            decodingDictionairy[alphabet[i]] = alphabet[(i-k)%len(alphabet)]
+
+        ciphertext = text
+        plaintext = ""
+
+        for cipherChar in ciphertext:
+            plaintext += decodingDictionairy[cipherChar]
+
+        return plaintext
+
+
+
+
+def vigenere(text, alphabet, k, code):
+    """text (string): The text you would like to encode or decode
+    alphabet (array[char]/string): List of all the available characters in order
+        May be one long string, or array of single chars
+    k (array[int]): Array of integers to be used as keys.
+        Period is determined by the length of the array
+        No padding is apllied here
+    code (int): Whether you want to encode(0) or decode(1)"""
+
+    if code == 0:
+
+        plaintext = text
+        ciphertext = ""
+        counter = 0
+
+        for plainChar in plaintext:
+            charIndex = alphabet.index(plainChar)
+            ciphertext += alphabet[(charIndex + k[counter % len(k)]) % len(alphabet)]
+        
+            counter += 1
+
+        return ciphertext
+
+    elif code == 1:
+
+        ciphertext = text
+        plaintext = ""
+        counter = 0
+
+        for cipherChar in ciphertext:
+            charIndex = alphabet.index(cipherChar)
+            plaintext += alphabet[(charIndex - k[counter % len(k)]) % len(alphabet)]
+        
+            counter += 1
+
+        return plaintext
+
+# Coded by Lucas Emmes some time during quarantine
