9c2147a4b363011c61937a2b58e2fb973ef96941
LucasEmmes <79790657+LucasEmmes@users.noreply>
2021-03-03 19:28:20 +0100

    Added to crypto
    Added transposition and substitution ciphers. Now with working docstrings (wow :o)

---
+++ Cryptography Schtuff/substitution.py
@@ -0,0 +1,73 @@
+import random
+
+def simple_substitute(text, alphabet, code):
+    """plaintext (string): The message you want to encode
+    alphabet (dictionairy[char] = char): Key is plaintext char,
+        value is the substitute.
+        Enter the same alphabet for both encode and decode
+    code int: Whether to encode (0) or decode (1)
+    returns string"""
+
+    if code == 0:
+        
+        plaintext = text
+        ciphertext = ""
+
+        for plainChar in plaintext:
+            ciphertext += alphabet[plainChar]
+
+        return ciphertext
+
+    elif code == 1:
+
+        ciphertext = text
+        plaintext = ""
+
+        #   Reverting alphabet
+        decodeAlphabet = {}
+        for key in alphabet.keys():
+            decodeAlphabet[alphabet[key]] = key
+
+        for cipherChar in ciphertext:
+            plaintext += decodeAlphabet[cipherChar]
+
+        return plaintext
+
+
+def poly_substitute(text, alphabet, code):
+    """plaintext (string): The message you want to encode
+    alphabet (dictionairy[char] = array[chars]): Key is plaintext char,
+        value is array of one or more substitute(s).
+        Enter the same alphabet for both encode and decode
+    code int: Whether to encode (0) or decode (1)
+    returns string"""
+    
+    if code == 0:   # Encode
+
+        plaintext = text
+        ciphertext = ""
+
+        for plainChar in plaintext:
+            #   Randomly picks a substitute
+            randomIndex = random.randint(0,len(alphabet[plainChar])-1)
+            ciphertext += alphabet[plainChar][randomIndex]
+
+        return ciphertext
+
+    elif code == 1: # Decode
+
+        ciphertext = text
+        plaintext = ""
+
+        #   Reverting alphabet
+        decodeAlphabet = {}
+        for key in alphabet.keys():
+            for sub in alphabet[key]:
+                decodeAlphabet[sub] = key
+
+        for cipherChar in ciphertext:
+            plaintext += decodeAlphabet[cipherChar]
+
+        return plaintext
+
+# Coded by Lucas Emmes some time during quarantine
