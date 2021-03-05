425c88d74b03b522ee6199e5c9396bd886e3fc13
LucasEmmes <79790657+LucasEmmes@users.noreply>
2021-03-05 11:39:53 +0100

    Removed Sudoku Solver v1.0 folder
    Moved all the relevant stuff to v1.1

--- Sudoku Solver v1.1/solver_only.py
+++ Sudoku Solver v1.1/solver_only.py
@@ -19,22 +19,27 @@ sudoku = sudoku_base.copy()
 # Added *args purely so that it can be started off
 # with solve() instead of solve(0). Looks nicer imo
 def solve(*args):
+    # Keeps track of where we are
     if len(args) == 0:
         n = 0
     else:
         n = args[0]
-    
+
+    # Checks if we have passed 81 tiles (i.e. we are done)
     if n == 81:
         sudoku_functions.print_sudoku(sudoku)
         return sudoku
     
     x = n%9
     y = n//9
-    
+
+    # Skips this tile if it was filled in by the base
     if sudoku_base[y][x] != 0:
         grid = solve(n+1)
         if grid:
             return grid
+    # Otherwise iterate through all allowed values
+    # and pass along after each iteration to test it
     else:
         taken = sudoku_functions.get_taken(x,y,sudoku)
         for i in range(1,10):
@@ -43,9 +48,12 @@ def solve(*args):
                 grid = solve(n+1)
                 if grid:
                     return grid
-                
+
+    # If we reach this point, it means we messed up earlier
+    # Remove current guesses and try with different previous values
     sudoku[y][x] = 0
 
+    # If we fail at tile 0, the sudoku is unsolvable
     if n == 0:
         print("Could not solve")
     return False
